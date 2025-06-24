e219ad586e44402a5a9a5e63d1f455efb3fa3a53
Egor587 <egorgusev880@gmail.com>
2025-06-24 15:04:22 +0300

    Реализовал оценку времени работы алгоритма

---
+++ ConsoleApplication24/ConsoleApplication24/main1.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "algoritm1.h"
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    system_sort();
+    return 0;
+}
